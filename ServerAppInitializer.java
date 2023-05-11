fbebae85fd5202cc9640f4f962d2d737791bc5d0
Ansh Mehrotra <mehrotra.ansh2@gmail.com>
2023-05-12 00:19:21 +0530

    updated

---
+++ ServerAppInitializer.java
@@ -0,0 +1,45 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ServerAppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("com/TCP/socketProgramming/views/ServerManagement.fxml")));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        scene.setFill(Color.TRANSPARENT);
+        enableMove(scene, primaryStage);
+        primaryStage.show();
+    }
+
+    private void enableMove(Scene scene, Stage primaryStage) {
+        AtomicReference<Double> xOffset = new AtomicReference<>((double) 0);
+        AtomicReference<Double> yOffset = new AtomicReference<>((double) 0);
+        scene.setOnMousePressed(event -> {
+            xOffset.set(primaryStage.getX() - event.getScreenX());
+            yOffset.set(primaryStage.getY() - event.getScreenY());
+        });
+        //Lambda mouse event handler
+        scene.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() + xOffset.get());
+            primaryStage.setY(event.getScreenY() + yOffset.get());
+        });
+    }
+}
